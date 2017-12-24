64eb142e82983331e352f14d3ece2a3f9e0bbc56
shuiyuwusong <disdain@aliyun.com>
2017-12-25 07:39:26 +0800

    03春杰提交作业

---
+++ Hibernate/T7/03春杰/上机作业/HBM_T7/src/com/qhit/lh/gr3/cj/t6/dao/BaseDao.java
@@ -0,0 +1,45 @@
+package com.qhit.lh.gr3.cj.t6.dao;
+
+import java.util.List;
+
+/**
+ * @author 03春杰
+ * TODO
+ * 2017年12月20日上午9:37:01	
+ */
+public interface BaseDao {
+	/**
+	 * 
+	 * @param object
+	 * 添加数据
+	 */
+	public void add(Object object);
+	/**
+	 * 
+	 * @param object
+	 * 删除数据
+	 */
+	public void delete(Object object);
+	/**
+	 * 
+	 * @param object
+	 * 修改数据
+	 */
+	public void update(Object object);
+	/**
+	 * 
+	 * @param formtable
+	 * @return
+	 * 查询数据
+	 */
+	public List<Object> query(String formtable);
+	
+	/**
+	 * @param obj
+	 * @param id
+	 * @return
+	 * 根据id去查
+	 */
+	@SuppressWarnings("rawtypes")
+	public Object getObjectById(Class clazz, int id);
+}
